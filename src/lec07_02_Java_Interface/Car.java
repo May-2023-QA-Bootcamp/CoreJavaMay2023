b5c4c75bf654c861f9dcf437dd7e79cdd58c0e82
TofaelKabir <tofaelkabir@gmail.com>
2023-05-29 19:46:02 -0400

    Java Interface

---
+++ src/lec07_02_Java_Interface/Car.java
@@ -0,0 +1,24 @@
+package lec07_02_Java_Interface;
+
+// Interface names starts with UpperCase 
+// They follow Camel case or Snake case pattern.
+// CamelCase or Snake_case feature considered from second word, not from first word
+// CamelCase example for an Interface --> CarToyota
+// Snake_case example for an Interface --> Car_toyota
+
+// What is Interface?
+// Interface is a Blueprint of the Class.
+// An interface is a collection of methods that are defined (declared) but not implemented. 
+
+public interface Car {
+	// Interface doesn't have method body like classes
+	// and here method is declared inside Interface body, 
+	// method declared, but not implemented
+	public void start ();
+	public void stop ();
+	public void brake ();
+	
+	// I am not initializing interface today
+	// Also not showing a relation between class and Interface
+
+}
---
+++ src/lec07_02_Java_Interface/Toyota.java
@@ -0,0 +1,11 @@
+package lec07_02_Java_Interface;
+
+// A regular class
+public class Toyota {
+	// method implemented, but not declared or defined
+	// This is a very very important interview question
+	public void toyotaInfo() {
+		System.out.println("This method is from Toyota Class");
+	}
+
+}
