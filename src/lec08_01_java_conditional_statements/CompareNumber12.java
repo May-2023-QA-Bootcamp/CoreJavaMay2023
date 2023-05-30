53e025544587e73687be3eabd55fd2c0a611b2d6
TofaelKabir <tofaelkabir@gmail.com>
2023-05-29 23:32:23 -0400

    Java Conditional statement

---
+++ src/lec08_01_java_conditional_statements/CompareNumber12.java
@@ -0,0 +1,50 @@
+package lec08_01_java_conditional_statements;
+
+import java.util.Scanner;
+
+// We will solve this issue in the next class
+public class CompareNumber12 {
+	// Why static? Non static method or variable can't be called inside main method
+	static int val1;
+	static int val2;
+		
+	// If val1 < val2, multiply val1 with val2
+	public static void multiplication () { 
+		int mult = val1*val2;
+		System.out.println("Multiplication is: " + mult);
+	}
+	
+	// If val1>val2, divide val1 by val2
+	public static void division () {
+		int div = val1/val2;
+		System.out.println("Division is: " + div);
+	}
+	
+	// If val1 == val2, do an addition of val1 and val2
+	public static void addition () {
+		int add = val1 + val2;
+		System.out.println("Addition is: " + add);
+	}
+	
+	public static void solution() {
+		if (val1 < val2) {
+			multiplication();
+		} else if (val1 > val2) {
+			division();
+		} else if (val1 == val2) {
+			addition();
+		}
+	}
+	
+	// if condition should be inside method, it can' be free, like you can not write condition in line 62
+
+	public static void main(String[] args) {
+		System.out.println("---: Please put your numbers below :---");
+		Scanner scanner = new Scanner(System.in);
+		int val1 = scanner.nextInt();
+		int val2 = scanner.nextInt();
+			
+		solution();		
+		scanner.close();
+	}
+}
